a939c8c94e6e2050da7c47dcc21b8e0f8863bab0
Oregu <oleg.burykin@gmail.com>
2014-03-16 21:31:03 +0400

    Fixed several warnings

--- src/Pakkuman.hs
+++ src/Pakkuman.hs
@@ -33,7 +33,7 @@ reshape s@(Size w h) = do
 	viewport $= (Position 0 0, s)
 	loadIdentity
 	let
-		aspect = realToFrac $ fromIntegral w / fromIntegral h
+		aspect = realToFrac $ fromIntegral w / (fromIntegral h :: Double)
 		border = realToFrac $ sceneSize/2.0
 	if w > h
 		then ortho2D 0 (border*aspect) border 0
@@ -76,22 +76,22 @@ nextHeroStamp (s, d) | d == 1  && s < 6  = (s+1,  1)
 
 collide :: [Sprite] -> Point2D -> Point2D
 collide lvl p@(x, y) = checkX . checkY $ p where
-	checkX (x, y)
+	checkX (xx, yy)
 		| heroLeft >= quadSize * fromIntegral(tx - 1) + minX ltile &&
 			heroLeft < quadSize * fromIntegral(tx - 1) + maxX ltile
 		= (cheeseheadRadius + quadSize * fromIntegral(tx - 1) + maxX ltile, y)
 		| heroRight > quadSize * fromIntegral(tx + 1) + minX rtile &&
 			heroRight <= quadSize * fromIntegral(tx + 1) + maxX rtile
 		= (quadSize * fromIntegral(tx + 1) - cheeseheadRadius + minX rtile, y)
-		| otherwise = (x, y)
-	checkY (x, y)
+		| otherwise = (xx, yy)
+	checkY (xx, yy)
 		| heroUp >= quadSize * fromIntegral(ty-1) + minY utile &&
 			heroUp < quadSize * fromIntegral(ty-1) + maxY utile
 		= (x, quadSize * fromIntegral(ty-1) + cheeseheadRadius + maxY utile)
 		| heroDown > quadSize * fromIntegral(ty+1) + minY dtile &&
 			heroDown <= quadSize * fromIntegral(ty+1) + maxY dtile
 		= (x, quadSize * fromIntegral(ty+1) - cheeseheadRadius + minY dtile)
-		| otherwise = (x, y)
+		| otherwise = (xx, yy)
 	(tx, ty) = point2tile (x, y)
 	left = ty * levelWidth + tx - 1
 	ltile = lvl !! left
